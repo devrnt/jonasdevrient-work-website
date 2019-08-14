690b79147510ab83b61b097c2d53bb9bcf12cd6d
devrnt <devrient.jonas@gmail.com>
2019-08-14 13:57:12 +0200

    Run format

--- src/templates/work-item.js
+++ src/templates/work-item.js
@@ -1,67 +1,66 @@
-import React from "react"
-import { graphql, Link } from 'gatsby';
+import React from 'react'
+import { graphql, Link } from 'gatsby'
 import { Layout, Container } from 'Common'
-import { IntroWrapper, Details } from "../components/landing/Intro/styles";
-import { SEO } from "../components/common/SEO";
-import goBack from '../../static/icons/go-back.svg';
+import { IntroWrapper, Details } from '../components/landing/Intro/styles'
+import { SEO } from '../components/common/SEO'
+import goBack from '../../static/icons/go-back.svg'
 
-
-export default ({data}) => {
-  const project = data.github.repositoryOwner.repository;
-  let readmeHtml = '';
-  if(data.githubReadme !== null){
-    readmeHtml = data.githubReadme.childMarkdownRemark.html;
+export default ({ data }) => {
+  const project = data.github.repositoryOwner.repository
+  let readmeHtml = ''
+  if (data.githubReadme !== null) {
+    readmeHtml = data.githubReadme.childMarkdownRemark.html
   }
 
-  let dom = ``;
+  let dom = ``
   // Wrap the require in check for window
   if (typeof window !== `undefined`) {
-    dom = document.createElement('div');
-    dom.innerHTML = readmeHtml;
-    
-    const images = dom.getElementsByTagName('img');
-    for(let element of images){
-      if(element.src.endsWith('.gif')){
-        const srcEnd = element.src.split('/showcase/')[1];
-        const beginUrl = `https://raw.github.com/devrnt/${project.name}/master/showcase/`;
-        const updatedSrc = `${beginUrl}${srcEnd}`;
-        element.src = updatedSrc;
-      } 
+    dom = document.createElement('div')
+    dom.innerHTML = readmeHtml
+
+    const images = dom.getElementsByTagName('img')
+    for (let element of images) {
+      if (element.src.endsWith('.gif')) {
+        const srcEnd = element.src.split('/showcase/')[1]
+        const beginUrl = `https://raw.github.com/devrnt/${project.name}/master/showcase/`
+        const updatedSrc = `${beginUrl}${srcEnd}`
+        element.src = updatedSrc
+      }
+    }
   }
-  }
-   
-  const title = `${project.name[0].toUpperCase()}${project.name.replace('-', ' ').substring(1)} | Jonas De Vrient `
+
+  const title = `${project.name[0].toUpperCase()}${project.name
+    .replace('-', ' ')
+    .substring(1)} | Jonas De Vrient `
 
   return (
     <>
-    <SEO
-     title={title}
-     description={project.description}
-     />
-    <Layout>
-      <IntroWrapper as={Container}>
-        <Details>
-        <Link to="/#projects">
-			{/* Jonas De Vrient */}
-			<img
-				src={goBack} 
-				alt="Ga terug"
-				style={{
-					width: `2.5rem`,
-					height: `2.5rem`,
-					marginBottom: `2.5rem`
-				}} />
-		</Link>
-          <div dangerouslySetInnerHTML={{__html: dom.innerHTML}}></div>
-        </Details>
-      </IntroWrapper>
-    </Layout>
+      <SEO title={title} description={project.description} />
+      <Layout>
+        <IntroWrapper as={Container}>
+          <Details>
+            <Link to="/#projects">
+              {/* Jonas De Vrient */}
+              <img
+                src={goBack}
+                alt="Ga terug"
+                style={{
+                  width: `2.5rem`,
+                  height: `2.5rem`,
+                  marginBottom: `2.5rem`,
+                }}
+              />
+            </Link>
+            <div dangerouslySetInnerHTML={{ __html: dom.innerHTML }}></div>
+          </Details>
+        </IntroWrapper>
+      </Layout>
     </>
-  );
+  )
 }
 
 export const query = graphql`
-  query ($name: String!) {
+  query($name: String!) {
     github {
       repositoryOwner(login: "devrnt") {
         repository(name: $name) {
@@ -75,7 +74,7 @@ export const query = graphql`
         }
       }
     }
-    githubReadme(title: {eq: $name}) {
+    githubReadme(title: { eq: $name }) {
       childMarkdownRemark {
         html
       }
