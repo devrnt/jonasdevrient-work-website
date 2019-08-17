51be80241919ed1e0672d7ea61d812e0a3e7d2d2
devrnt <devrient.jonas@gmail.com>
2019-08-17 19:39:36 +0200

    Add optimizations

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,6 @@ module.exports = {
         path: '${__dirname}/src/assets',
       },
     }, */
-		'gatsby-transformer-sharp',
 		'gatsby-plugin-sharp',
 		{
 			resolve: 'gatsby-source-graphql',
@@ -95,6 +94,49 @@ module.exports = {
 				theme_color: config.themeColor,
 				display: 'standalone',
 				icon: './static/logo.png',
+				include_favicon: true,
+				icons: [
+          {
+            "src": "favicons/icon-48x48.png",
+            "sizes": "48x48",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+        ],
 			},
 		},
 		'gatsby-plugin-offline',
